0f4aa055774d5dd67a55cbf7cac61ce132bb8e59
Eugene <68151908+EugeneI61@users.noreply.github.com>
2022-02-16 11:50:41 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/FileManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class FileManager
+    {
+        public string PathLine;
+        public static void Find(string pathLine)
+        {
+            Console.WriteLine("Input a word for find!");
+            string findWord = Console.ReadLine();
+
+            string readText = File.ReadAllText(pathLine);
+
+            char[] separator = new char[] { ' ', '.', ',', '!', '?', '-' };
+
+            string[] splitedText = readText.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            int wordsCounter = 0;
+
+            for (int i = 0; i < splitedText.Length; i++)
+            {
+                if (String.Equals(splitedText[i], findWord, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    wordsCounter++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("Count: " + wordsCounter);
+            Console.WriteLine();
+        }
+
+        public static void Delete(string pathLine)
+        {
+            Console.WriteLine("Input a word for Deleting!");
+            string deleteWord = Console.ReadLine();
+            Console.WriteLine();
+
+            int deleteCounter = 0;
+
+            string str = String.Empty;
+
+            using (StreamReader reader = File.OpenText(pathLine))
+            {
+                str = reader.ReadToEnd();
+            }
+
+            char[] separator = new char[] { ' ', '.', ',', '!', '?', '-' };
+
+            string[] splitedText = str.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < splitedText.Length; i++)
+            {
+                if (String.Equals(splitedText[i], deleteWord, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    str = str.Replace(deleteWord, "", StringComparison.CurrentCultureIgnoreCase);
+                    deleteCounter++;
+                }
+            }
+
+            using (StreamWriter file = new StreamWriter(pathLine))
+            {
+                file.Write(str);
+            }
+
+            Console.WriteLine("Was delete: " + deleteCounter);
+            Console.WriteLine();
+        }
+
+        public static void Replace(string pathLine)
+        {
+            Console.WriteLine("Input a word for Replace");
+            string replaceWord = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Input a word for Write!");
+            Console.WriteLine();
+            string writeWord = Console.ReadLine();
+
+            string str = String.Empty;
+            using (StreamReader reader = File.OpenText(pathLine))
+            {
+                str = reader.ReadToEnd();
+            }
+
+            str = str.Replace(replaceWord, writeWord, StringComparison.CurrentCultureIgnoreCase);
+
+            using (StreamWriter file = new StreamWriter(pathLine))
+            {
+                file.Write(str);
+            }
+        }
+        public FileManager(string pathLine)
+        {
+            this.PathLine = pathLine;
+        }
+
+    }
+}
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,90 +1,13 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace ConsoleApp2
 {
     class Program
     {
-        static void Find(string pathLine)
-        {
-            Console.WriteLine("Input a word for find!");
-            string findWord = Console.ReadLine();
-
-            string readText = File.ReadAllText(pathLine);
-
-            char[] separator = new char[] { ' ', '.', ',', '!', '?', '-' };
-
-            string[] splitedText = readText.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-
-            int wordsCounter = 0;
-
-            for (int i = 0; i < splitedText.Length; i++)
-            {
-                //Console.WriteLine(splitedText[i]);
-                if (string.Equals(splitedText[i], findWord, StringComparison.CurrentCultureIgnoreCase))
-                {
-                    wordsCounter++;
-                }
-                else
-                {
-                    continue;
-                }
-            }
-            Console.WriteLine("Count: " + wordsCounter);
-            Console.WriteLine();
-        }
-
-        static void Delete(string pathLine)
-        {
-            Console.WriteLine("Input a word for Delete!");
-            string deleteWord = Console.ReadLine();
-            Console.WriteLine();
-
-            string str = string.Empty;
-            using (System.IO.StreamReader reader = System.IO.File.OpenText(pathLine))
-            {
-                str = reader.ReadToEnd();
-            }
-
-            str = str.Replace(deleteWord, "", StringComparison.CurrentCultureIgnoreCase);
-
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(pathLine))
-            {
-                file.Write(str);
-            }
-        }
-
-        static void Replace(string pathLine)
-        {
-            Console.WriteLine("Input a word for Replace");
-            string replaceWord = Console.ReadLine();
-            Console.WriteLine();
-            Console.WriteLine("Input a word for Write!");
-            Console.WriteLine();
-            string writeWord = Console.ReadLine();
-
-            string str = string.Empty;
-            using (System.IO.StreamReader reader = System.IO.File.OpenText(pathLine))
-            {
-                str = reader.ReadToEnd();
-            }
-
-            str = str.Replace(replaceWord, writeWord, StringComparison.CurrentCultureIgnoreCase);
-
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(pathLine))
-            {
-                file.Write(str);
-            }
-
-        }
-
         static void Main(string[] args)
         {
             string pathLine;
-            // string findWord;
             while (true)
             {
                 Console.WriteLine("Input path to file!");
@@ -103,14 +26,13 @@ namespace ConsoleApp2
                     continue;
                 }
             }
-
             //D:\Words.txt
-            //sit
-            //Faucibus
-
+            
             char choise;
             do
             {
+                FileManager fm = new FileManager(pathLine);
+
                 Console.WriteLine("Commands: " + "\n" + "***********************");
                 Console.WriteLine("f - find word;");
                 Console.WriteLine("r - replace word;");
@@ -120,6 +42,8 @@ namespace ConsoleApp2
                 Console.WriteLine("Make a chose!");
                 Console.WriteLine();
                 string smallChoise = Console.ReadLine();
+
+                
                 if (Char.TryParse(smallChoise, out choise) && (choise == 'r' || choise == 'f' || choise == 'd'))
                 {
                     Console.WriteLine("Good!");
@@ -132,14 +56,13 @@ namespace ConsoleApp2
                 switch (choise)
                 {
                     case 'r':
-                        Replace(pathLine);
+                        FileManager.Replace(fm.PathLine);
                         break;
                     case 'd':
-                        Delete(pathLine);
+                        FileManager.Delete(fm.PathLine);
                         break;
-
                     case 'f':
-                        Find(pathLine);
+                        FileManager.Find(fm.PathLine);
                         break;
                     default:
                         Console.WriteLine("RT");
