aad602a86cb5bae1d8d421d847d7558162ca85ed
EugeneI61 <zenakovalchuk@gmail.com>
2022-04-04 11:35:45 +0300

    d

--- ClientApp/task8/src/App.js
+++ ClientApp/task8/src/App.js
@@ -57,7 +57,7 @@ export default function App() {
         alert(error);
       });
   }
-//https://abcinblog.blogspot.com/2019/02/react-i.html
+  //https://abcinblog.blogspot.com/2019/02/react-i.html
   function deleteEmployee(deleteId) {
 
     const url = 'https://localhost:44310/api/employees/' + Number.parseInt(deleteId);
@@ -67,12 +67,47 @@ export default function App() {
     })
       .then(response => response.json())
       .then(responseFromServer => {
-        console.log(responseFromServer); onEmployeeDeleted();
+        console.log(responseFromServer);
+        alert("Employee was Delete!");
       })
       .catch((error) => {
         console.log(error);
       });
   }
+
+  const useSortableData = (items, config = null) => {
+    const [sortConfig, setSortConfig] = React.useState(config);
+
+    const sortedItems = React.useMemo(() => {
+      let sortableItems = [...items];
+      if (sortConfig !== null) {
+        sortableItems.sort((a, b) => {
+          if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? -1 : 1;
+          }
+          if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+      return sortableItems;
+    }, [items, sortConfig]);
+
+    const requestSort = (key) => {
+      let direction = 'ascending';
+      if (
+        sortConfig &&
+        sortConfig.key === key &&
+        sortConfig.direction === 'ascending'
+      ) {
+        direction = 'descending';
+      }
+      setSortConfig({ key, direction });
+    };
+
+    return { items: sortedItems, requestSort, sortConfig };
+  };
 
   return (
     <div className="container">
@@ -89,7 +124,7 @@ export default function App() {
           )}
           {(showAddNewEmployeeForm && employee.length > 5) && Overflow()}
 
-          {(employee.length > 0 && showAddNewEmployeeForm === false) && renderTable()}
+          {(employee.length > 0 && showAddNewEmployeeForm === false) && RenderTable()}
 
           {showAddNewEmployeeForm && <EmployeeAddForm onEmployeeAdded={onEmployeeAdded} />}
         </div>
@@ -97,18 +132,30 @@ export default function App() {
     </div>
   );
 
-  function renderTable() {
+  function RenderTable(props) {
+    const { items, requestSort, sortConfig } = useSortableData(props.employee);
+    const getClassNamesFor = (name) => {
+      if (!sortConfig) {
+        return;
+      }
+      return sortConfig.key === name ? sortConfig.direction : undefined;
+    };
     return (
-      <div className="table-responsive mt-5">
+      <div className="table-responsive">
         <input className="form-control mt-4" type="text" placeholder="Filter" id="search-text" onKeyUp={tableSearch}></input>
-        <table className="table table-bordered border-dark" id="info-table">
+        <table className="table" id="info-table">
           <thead>
             <tr>
-              <th scope="col">EmployeeId</th>
-              <th scope="col">EmployeeName</th>
-              <th scope="col">EmployeeAge</th>
-              <th scope="col">Car</th>
-              <th scope="col">RecordId</th>
+              <th ><button type="button" onClick={() => requestSort('employeeId')}
+                className={getClassNamesFor('employeeId')}>Id</button></th>
+              <th ><button type="button" onClick={() => requestSort('employeeName')}
+                className={getClassNamesFor('employeeName')}>EmployeeName </button></th>
+              <th ><button type="button" onClick={() => requestSort('employeeAge')}
+                className={getClassNamesFor('employeeAge')}>EmployeeAge</button></th>
+              <th ><button type="button" onClick={() => requestSort('car')}
+                className={getClassNamesFor('car')}>Car</button></th>
+              <th ><button type="button" onClick={() => requestSort('recordId')}
+                className={getClassNamesFor('recordId')}>RecordId</button></th>
             </tr>
           </thead>
           <tbody>
@@ -123,7 +170,6 @@ export default function App() {
             ))}
           </tbody>
         </table>
-
         <button onClick={() => setShowAddNewEmployeeForm(true)} className="btn btn-secondary btn-lg w-100 mt-4">Add New Employee</button>
         <button onClick={() => setEmployee([])} className="btn btn-secondary btn-lg w-100 mt-4">Empty React Array</button>
         <div className='mt-4'>
@@ -138,41 +184,40 @@ export default function App() {
         </div>
       </div>
     );
-  }
+  };
 
-  function onEmployeeAdded(addedEmployee) {
-    setShowAddNewEmployeeForm(false);
-    if (addedEmployee === null) {
-      return;
-    }
-    alert("Post Success!");
-    getEmployees();
-  }
 
-  function Overflow() {
-    setShowAddNewEmployeeForm(false);
-    alert("Data is Overflow!");
-  }
-  function onEmployeeDeleted() {
-    alert("Employee successfully deleted!");
-  }
-  function tableSearch() {
-    var phrase = document.getElementById('search-text');
-    var table = document.getElementById('info-table');
-    var regPhrase = new RegExp(phrase.value, 'i');
-    var flag = false;
-    for (var i = 1; i < table.rows.length; i++) {
-      flag = false;
-      for (var j = table.rows[i].cells.length - 1; j >= 0; j--) {
-        flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
-        if (flag) break;
+    function onEmployeeAdded(addedEmployee) {
+      setShowAddNewEmployeeForm(false);
+      if (addedEmployee === null) {
+        return;
       }
-      if (flag) {
-        table.rows[i].style.display = "";
-      } else {
-        table.rows[i].style.display = "none";
+      alert("Post Success!");
+      getEmployees();
+    }
+
+    function Overflow() {
+      setShowAddNewEmployeeForm(false);
+      alert("Data is Overflow!");
+    }
+    function tableSearch() {
+      var phrase = document.getElementById('search-text');
+      var table = document.getElementById('info-table');
+      var regPhrase = new RegExp(phrase.value, 'i');
+      var flag = false;
+      for (var i = 1; i < table.rows.length; i++) {
+        flag = false;
+        for (var j = table.rows[i].cells.length - 1; j >= 0; j--) {
+          flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+          if (flag) break;
+        }
+        if (flag) {
+          table.rows[i].style.display = "";
+        } else {
+          table.rows[i].style.display = "none";
+        }
       }
     }
+
   }
-}
 
